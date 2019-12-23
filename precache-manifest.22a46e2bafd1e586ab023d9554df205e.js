e9286df14ff4eb7455fb3a0778f041441999f651
hibiyo <hanserku@outlook.com>
2019-12-23 17:47:22 +0800

    Updates

---
+++ precache-manifest.22a46e2bafd1e586ab023d9554df205e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91ff4209a20eab864635f8e76ba0e1fe",
+    "url": "/hibiyo/shop-cart/index.html"
+  },
+  {
+    "revision": "2ba7193449805f3b69a6",
+    "url": "/hibiyo/shop-cart/static/css/main.7c335f61.chunk.css"
+  },
+  {
+    "revision": "9c5039090901758368ee",
+    "url": "/hibiyo/shop-cart/static/js/2.8ba5229e.chunk.js"
+  },
+  {
+    "revision": "2ba7193449805f3b69a6",
+    "url": "/hibiyo/shop-cart/static/js/main.b9fcf435.chunk.js"
+  },
+  {
+    "revision": "4f390bb1bdfaf117e6db",
+    "url": "/hibiyo/shop-cart/static/js/runtime-main.b636f681.js"
+  }
+]);
